9a0579758150797a461e8409270506443099d7d7
Khusinov <shaxriyorqin@gmail.com>
2022-02-09 18:54:38 +0500

    finished fragments

--- app/src/main/java/uz/khusinov/navigationcomponentsexample/ViewBalanceFragment.kt
+++ app/src/main/java/uz/khusinov/navigationcomponentsexample/ViewBalanceFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class ViewBalancceFragment : Fragment() {
+class ViewBalanceFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_balancce, container, false)
+        return inflater.inflate(R.layout.fragment_view_balance, container, false)
     }
 
 }
